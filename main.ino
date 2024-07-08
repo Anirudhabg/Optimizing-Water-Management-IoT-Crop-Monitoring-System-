29c55b72c1493c8e88cca04c6cfff9b38d146931
Anirudha B G Somayaji <anirudhabg@gmail.com>
2024-07-08 15:42:13 +0530

    Made Chanegs

--- main.ino
+++ main.ino
@@ -1,28 +1,27 @@
 // Blynk authentication token and template details
-#define BLYNK_TEMPLATE_ID "your_template_id"
-#define BLYNK_TEMPLATE_NAME "your_template_name"
-#define BLYNK_AUTH_TOKEN "your_auth_token"
-
+#define BLYNK_TEMPLATE_ID "YOUR_TEMPLATE_ID"
+#define BLYNK_TEMPLATE_NAME "YOUR_TEMPLATE_NAME"
+#define BLYNK_AUTH_TOKEN "YOUR_TEMPLATE_AUTH"
 // Include necessary libraries
-#include <BlynkSimpleEsp8266.h> // Blynk library
-#include <ESP8266WiFi.h>        // ESP8266 WiFi library
-#include <Wire.h>               // Wire library (for I2C)
-#include <DHT.h>                // DHT11 Library
+#include <BlynkSimpleEsp8266.h>  // Blynk library
+#include <ESP8266WiFi.h>         // ESP8266 WiFi library
+#include <Wire.h>                // Wire library (for I2C)
+#include <DHT.h>                 // DHT11 Library
 
-#define BLYNK_PRINT Serial // Blynk debug prints to Serial monitor
+#define BLYNK_PRINT Serial  // Blynk debug prints to Serial monitor
 
-#define MUX_S0 D1  // Pin S0 of 74HC4051 connected to NodeMCU D1
-#define MUX_S1 D2  // Pin S1 of 74HC4051 connected to NodeMCU D2
-#define MUX_S2 D5  // Pin S2 of 74HC4051 connected to NodeMCU D5
-#define MUX_SIG A0 // Analog pin connected to SIG pin of 74HC4051
+#define MUX_S0 D1   // Pin S0 of 74HC4051 connected to NodeMCU D1
+#define MUX_S1 D2   // Pin S1 of 74HC4051 connected to NodeMCU D2
+#define MUX_S2 D5   // Pin S2 of 74HC4051 connected to NodeMCU D3
+#define MUX_SIG A0  // Analog pin connected to SIG pin of 74HC4051
 
 // Define sensor pins
-#define WATER_PUMP_PIN D7 // Water Pump
-#define DHT_PIN D4        // DHT11 Sensor
+#define WATER_PUMP_PIN D7  // Water Pump
+#define DHT_PIN D6         // DHT11 Sensor
 
 // WiFi credentials
-char ssid[] = "********"; // Your WiFi SSID
-char pass[] = "********"; // Your WiFi password
+char ssid[] = "********";     // Your WiFi SSID
+char pass[] = "********";  // Your WiFi password
 char auth[] = BLYNK_AUTH_TOKEN;
 
 // Blynk timer
@@ -32,156 +31,142 @@ BlynkTimer timer;
 DHT dht(DHT_PIN, DHT11);
 
 // Thresholds and calibration values
-float soilThresholdValue = 25.0;                    // 25% water content
-float rainThresholdValue = 60.0;                    // 60% Rain
-float pHThresholdLow = 6.0;                         // Minimum acceptable pH
-float pHThresholdHigh = 8.5;                        // Maximum acceptable pH
-float calibrationSlope = 1.6512113655911938;        // Calculated pH calibration slope
-float calibrationIntercept = 0.0014312808810704651; // Calculated pH calibration intercept
+float soilThresholdValue = 25.0;                     // 30% water content
+float rainThresholdValue = 60.0;                     // 60% Rain
+float pHThresholdLow = 6.0;                          // Minimum acceptable pH
+float pHThresholdHigh = 8.5;                         // Maximum acceptable pH
+float calibrationSlope = 1.6512113655911938;         // pH calibration slope
+float calibrationIntercept = 0.0014312808810704651;  // pH calibration intercept
 
 // Setup function
-void setup()
-{
-    Serial.begin(9600);
-    // Connect to WiFi and Blynk
-    Blynk.begin(auth, ssid, pass);
-    Serial.println("Connected to Blynk!!!");
-    dht.begin();
+void setup() {
+  Serial.begin(9600);
+   ESP.wdtEnable(40000);
+  Serial.println("Resetting!!");
+delay(1000);
+  // Connect to WiFi and Blynk
+  Blynk.begin(auth, ssid, pass);
+  Serial.println("Connected to Blynk!!!");
+  dht.begin();
 
-    pinMode(MUX_S0, OUTPUT);
-    pinMode(MUX_S1, OUTPUT);
-    pinMode(MUX_S2, OUTPUT);
+  pinMode(MUX_S0, OUTPUT);
+  pinMode(MUX_S1, OUTPUT);
+  pinMode(MUX_S2, OUTPUT);
 
-    pinMode(WATER_PUMP_PIN, OUTPUT);
-    digitalWrite(WATER_PUMP_PIN, LOW); // Ensure water pump is off initially
+  pinMode(WATER_PUMP_PIN, OUTPUT);
+  digitalWrite(WATER_PUMP_PIN, HIGH);  // Ensure water pump is off initially
 
-    timer.setInterval(1000L, MainFunction);
-    timer.setInterval(2000L, DHT11Sensor);
+  timer.setInterval(1000L, MainFunction);
+  timer.setInterval(2000L, DHT11Sensor);
 }
 
-void DHT11Sensor()
-{
-    // Read temperature and humidity from DHT sensor
-    float humidity = dht.readHumidity();
-    float temperature = dht.readTemperature();
+void DHT11Sensor() {
+  // Read temperature and humidity from DHT sensor
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
 
-    // Check for valid readings
-    if (isnan(humidity) || isnan(temperature))
-    {
-        Serial.println("Failed to read from DHT sensor!");
-        return;
+  // Check for valid readings
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Blynk.virtualWrite(V0, temperature);
+  Blynk.virtualWrite(V1, humidity);
+}
+
+void MainFunction() {
+  float soilMoistureSensorValue = calculateSoilMoistureValue();
+  float pHSensorValue = calculatepHSensorValue();
+  float rainSensorValue = calculateRainSensorValue();
+
+  Serial.print("Soil Moisture Value: ");
+  Serial.println(soilMoistureSensorValue);
+  
+  Serial.print("pH Sensor Value: ");
+  Serial.println(pHSensorValue);
+  
+  Serial.print("Rain Sensor Value: ");
+  Serial.println(rainSensorValue);
+
+  Blynk.virtualWrite(V2, soilMoistureSensorValue);
+  Blynk.virtualWrite(V4, pHSensorValue);
+
+  // Check for Rain
+  if (rainSensorValue > rainThresholdValue) {
+    Blynk.logEvent("rain_alert", "Attention: Pump turned off due to heavy rain.");
+    Serial.println("Heavy Rain Alert!!!");
+    digitalWrite(WATER_PUMP_PIN, HIGH);  // Turn off the water pump
+    return;
+  }
+
+  // Check for Soil Moisture Content
+  if (soilMoistureSensorValue < soilThresholdValue) {
+    Blynk.logEvent("soil_alert", "Attention: Water content is too low. Please water the plants.");
+    Serial.println("Soil Moisture Content is Low!!!");
+
+    // Check for pH Levels
+    if (pHSensorValue < pHThresholdLow) {
+      Blynk.logEvent("phAcidic", "Alert: Water pH is below 6. Possible acidity detected. Please consider changing the water.");
+      Serial.println("Acidic Water Content!!!");
+
+      return;
+    } else if (pHSensorValue > pHThresholdHigh) {
+      Blynk.logEvent("pHBasic", "Alert: Water pH Level High! Possible basic content detected. Check water quality.");
+      Serial.println("Basic Water Content!!!");
+
+      return;
+    } else {
+      Serial.println("pH is normal so watering the plants");
+      digitalWrite(WATER_PUMP_PIN, LOW);
+      delay(10000);
+      digitalWrite(WATER_PUMP_PIN, HIGH);
     }
-
-    Blynk.virtualWrite(V0, temperature);
-    Blynk.virtualWrite(V1, humidity);
+  }
+  delay(1000);
 }
 
-void MainFunction()
-{
-    float soilMoistureSensorValue = calculateSoilMoistureValue();
-    float pHSensorValue = calculatepHSensorValue();
-    float rainSensorValue = calculateRainSensorValue();
-
-    Serial.print("Soil Moisture Value: ");
-    Serial.println(soilMoistureSensorValue);
-
-    Serial.print("pH Sensor Value: ");
-    Serial.println(pHSensorValue);
-
-    Serial.print("Rain Sensor Value: ");
-    Serial.println(rainSensorValue);
-
-    Blynk.virtualWrite(V2, soilMoistureSensorValue);
-    Blynk.virtualWrite(V4, pHSensorValue);
-
-    // Check for Rain
-    if (rainSensorValue > rainThresholdValue)
-    {
-        Blynk.logEvent("rain_alert", "Attention: Pump turned off due to heavy rain.");
-        Serial.println("Heavy Rain Alert!!!");
-        digitalWrite(WATER_PUMP_PIN, LOW); // Turn off the water pump
-        return;
-    }
-
-    // Check for Soil Moisture Content
-    if (soilMoistureSensorValue < soilThresholdValue)
-    {
-        Blynk.logEvent("soil_alert", "Attention: Water content is too low. Please water the plants.");
-        Serial.println("Soil Moisture Content is Low!!!");
-
-        // Check for pH Levels
-        if (pHSensorValue < pHThresholdLow)
-        {
-            Blynk.logEvent("phAcidic", "Alert: Water pH is below 6. Possible acidity detected. Please consider changing the water.");
-            Serial.println("Acidic Water Content!!!");
-
-            return;
-        }
-        else if (pHSensorValue > pHThresholdHigh)
-        {
-            Blynk.logEvent("pHBasic", "Alert: Water pH Level High! Possible basic content detected. Check water quality.");
-            Serial.println("Basic Water Content!!!");
-
-            return;
-        }
-        else
-        {
-            Serial.println("pH is normal so watering the plants");
-            while (soilMoistureSensorValue <= soilThresholdValue)
-            {
-                digitalWrite(WATER_PUMP_PIN, HIGH);
-                soilMoistureSensorValue = calculateSoilMoistureValue();
-            }
-        }
-    }
-    delay(1000);
+void selectMuxChannel(int channel) {
+  digitalWrite(MUX_S0, channel & 0x01);
+  digitalWrite(MUX_S1, (channel >> 1) & 0x01);
+  digitalWrite(MUX_S2, (channel >> 2) & 0x01);
 }
 
-void selectMuxChannel(int channel)
-{
-    digitalWrite(MUX_S0, channel & 0x01);
-    digitalWrite(MUX_S1, (channel >> 1) & 0x01);
-    digitalWrite(MUX_S2, (channel >> 2) & 0x01);
+int readSensor() {
+  int sensorValue = analogRead(MUX_SIG);
+  return sensorValue;
 }
 
-int readSensor()
-{
-    int sensorValue = analogRead(MUX_SIG);
-    return sensorValue;
+float calculateSoilMoistureValue() {
+  int soilChannel = 1;  // Select channel 1 (Y1) for soil moisture sensor
+  selectMuxChannel(soilChannel);
+  int moistureValue = readSensor();
+  // Map the moisture sensor value to a percentage (assuming 0-1023 range)
+  float mappedSoilMoistureValue = map(moistureValue, 0, 1023, 100, 0);
+  return mappedSoilMoistureValue;
 }
 
-float calculateSoilMoistureValue()
-{
-    int soilChannel = 1; // Select channel 1 (Y1) for soil moisture sensor
-    selectMuxChannel(soilChannel);
-    int moistureValue = readSensor();
-    // Map the moisture sensor value to a percentage (assuming 0-1023 range)
-    float mappedSoilMoistureValue = map(moistureValue, 0, 1023, 100, 0);
-    return mappedSoilMoistureValue;
+float calculatepHSensorValue() {
+  int pHChannel = 2;  // Select channel 2 (Y2) for ph sensor
+  selectMuxChannel(pHChannel);
+  int pHValue = readSensor();
+  // Serial.print(pHValue);
+  // Convert pH sensor value to actual pH value using calibration parameters
+  float voltage = (pHValue * 5.0) / 1023.0;
+  float mappedpHSensorValue = calibrationSlope * voltage + calibrationIntercept;
+  return mappedpHSensorValue;
 }
 
-float calculatepHSensorValue()
-{
-    int pHChannel = 2; // Select channel 2 (Y2) for ph sensor
-    selectMuxChannel(pHChannel);
-    int pHValue = readSensor();
-    // Convert pH sensor value to actual pH value using calibration parameters
-    float voltage = (pHValue * 5.0) / 1023.0;
-    float mappedpHSensorValue = calibrationSlope * voltage + calibrationIntercept;
-    return mappedpHSensorValue;
+float calculateRainSensorValue() {
+  int rainChannel = 0;  // Select channel 0 (Y0) for rain sensor
+  selectMuxChannel(rainChannel);
+  int rainValue = readSensor();
+  float mappedRainValue = map(rainValue, 0, 1024, 100, 0);
+  return mappedRainValue;
 }
 
-float calculateRainSensorValue()
-{
-    int rainChannel = 0; // Select channel 0 (Y0) for rain sensor
-    selectMuxChannel(rainChannel);
-    int rainValue = readSensor();
-    float mappedRainValue = map(rainValue, 0, 1024, 100, 0);
-    return mappedRainValue;
-}
-
-void loop()
-{
-    Blynk.run();
-    timer.run();
+void loop() {
+  
+  Blynk.run();
+  timer.run();
 }
